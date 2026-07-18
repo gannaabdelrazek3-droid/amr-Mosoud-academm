3922ab6efecce9b2ecd9a907a7a0559cd4a4900d
Ganna <gannaabdelrazek3@gmail.com>
2026-07-18 17:51:32 +0300

    fix supabase client syntax

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -4,9 +4,9 @@ import { cookies } from 'next/headers'
 export async function createClient() {
   const cookieStore = await cookies()
 
-  return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+ return createServerClient(
+    "https://yxobqgrhpnltbtjnqzwz.supabase.co",
+    "sb_publishable_41gsQ8FyUs53Ll7QT4Wl7Q_UPfCf_",
     {
       cookies: {
         getAll() {
